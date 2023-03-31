63d424e4fb9db28a794143ff94bfac4928ef2f2b
FabioArdi <arditfab@students.zhaw.ch>
2023-03-31 14:35:15 +0200

    Added new htmls

---
+++ Projekt_1/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, send_file
+from transformers import AutoFeatureExtractor, CvtForImageClassification
+from PIL import Image
+
+app = Flask(__name__)
+feature_extractor = AutoFeatureExtractor.from_pretrained('microsoft/cvt-13')
+model = CvtForImageClassification.from_pretrained('microsoft/cvt-13')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/temp/<filename>')
+def serve_image(filename):
+    return send_file(f'temp/{filename}', mimetype='image/jpeg')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    file = request.files['image']
+    image = Image.open(file.stream).convert('RGB')
+
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+    predicted_class_label = model.config.id2label[predicted_class_idx]
+
+
+    # Save the uploaded image to a temporary folder
+    temp_image_path = f"temp/{file.filename}"
+    image.save(temp_image_path)
+
+    print(temp_image_path)
+
+    # Pass the predicted class label and the temporary image path to the result page
+    return render_template('result.html', label=predicted_class_label, image_url=temp_image_path)
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=False)
